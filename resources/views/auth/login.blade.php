19be9c0d8fb6afc2edbb30fbe51012f7483a4419
Soyuuwu <phamgiann2006@gmail.com>
2026-03-16 00:52:21 +0700

    layout and testing!

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,3 +1,11 @@
 @extends('layouts.app') @section('title', 'Đăng ký tài khoản')
 
 @section('content')
+<style>
+    h1 {
+        text-align: center;    
+        color: blue;
+    }
+</style>
+<h1>login page</h1>
+@endsection
