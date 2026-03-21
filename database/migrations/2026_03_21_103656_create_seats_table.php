5e1e6649cc1371cf385e07cf41d5a565a7f5097d
Soyuuwu <phamgiann2006@gmail.com>
2026-03-21 19:14:38 +0700

    study about migrate and schema

---
+++ database/migrations/2026_03_21_103656_create_seats_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {   
+        if(!Schema::hasTable('seats')){
+            Schema::create('seats', function (Blueprint $table) {
+                $table->id();
+                $table->timestamps();
+                $table->text('area');
+                $table->unsignedInteger('cost');
+            });
+        }
+
+        Schema::table('seats', function (Blueprint $table) {
+            $table->integer('floor')->before('area');
+        });
+    }
+        
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('seats');
+    }
+};
