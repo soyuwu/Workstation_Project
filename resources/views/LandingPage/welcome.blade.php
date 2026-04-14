bcf681ae091191360b0d74ee68a4be859df5d6ce
VoDuyManh.github.io <vomanh462@gmail.com>
2026-04-14 08:57:35 +0700

    commit update

--- resources/views/LandingPage/welcome.blade.php
+++ resources/views/LandingPage/welcome.blade.php
@@ -1,14 +1,3 @@
 @extends('layouts.topBar')
 
 @section('page-title', 'Trang chủ')
-
-@section('content')
-      <div class="card">
-            <div class="card__header">
-                  <h2 class="card__title">Chào mừng đến với Workstation</h2>
-            </div>
-            <div style="color: #ffffffff; padding-top: 10px;">
-                  Vui lòng chọn một mục ở menu bên trái để tiếp tục quản lý hệ thống.
-            </div>
-      </div>
-@endsection
