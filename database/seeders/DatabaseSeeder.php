b5295f21e01318eaf99eb4135b2aa5efa4ef47db
soyuwu <phamgiann2006@gmail.com>
2026-05-24 12:55:04 +0700

    docker for deployment 3.0

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        User::firstOrCreate(
+            ['email' => 'phamgiann2006@gmail.com'],
+            [
+                'name' => 'Admin System',
+                'role' => 'admin',
+                'status' => 'active',
+                'password' => Hash::make('1234'),
+                'email_verified_at' => now(),
+            ]
+        );
         $this->call([
             RoomTypeSeeder::class,
             WorkspaceSeeder::class,
